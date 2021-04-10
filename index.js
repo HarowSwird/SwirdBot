612bd8d9e81081d0827da5109e17eb9e625bcbab
HarowSwird <82154217+HarowSwird@users.noreply.github.com>
2021-04-10 17:42:32 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,107 @@
+const Discord = require("discord.js");
+
+const Client = new Discord.Client;
+
+const prefix = "%";
+
+Client.on("ready", () => {
+    console.log("bot");
+});
+
+Client.on("guildMemberAdd", member => {
+    console.log("Un nouveau membre est arrivé");
+    //member.guild.channels.cache.find(channel => channel.id == "829401020617719818").send(member.displayName + "est arrivé !\nNous sommes désormais **" + member.guild.memberCount + "**sur le serveur");
+    member.roles.add("820336550105448458").then(mbr => {
+        console.log("Rôle attribué avec succès pour " + member.displayName);
+    }).catch(() => {
+        console.log("Le rôle n'a pas pu être attribué");
+    });
+});
+
+Client.on("guildMemberRemove", member => {
+    console.log("Un membre nous a quitté");
+    member.guild.channels.cache.find(channel => channel.id == "830081172779958312").send(member.displayName + " nous a quité !\nNous sommes désormais plus que **" + member.guild.memberCount + "** sur le serveur");
+});
+
+Client.on("message", message => {
+    if(message.author.bot) return;
+
+    var embed = new Discord.MessageEmbed()
+        .setColor("#A9258D")
+        .setTitle("**Sondage**" + " :bar_chart: :")
+        .addField("Pain au Chocolat ou Chocolatine ?", "1 - Pain au Chocolat \n------------\n2 - Chocolatine", false)
+        .setFooter("Created by Harow | v.1")
+
+    message.channel.send(embed);
+});
+
+Client.on("message", message => {
+    if(message.member.permissions.has("MANAGE_MESSAGES")){
+        if(message.content.startsWith(prefix + "clear")){
+            let args = message.content.split(" ");
+
+            if(args[1] == undefined){
+                message.reply("Nombre de message non ou mal défini");
+            }
+            else {
+                let number = parseInt(args[1]);
+                
+                if(isNaN(number)){
+                    message.reply("Nombre de message non ou mal défini");
+                }
+                else {
+                    message.channel.bulkDelete(number).then(messages => {
+                        console.log("Supression de " + messages.size + "messages réussi !")
+                    }).catch(err => {
+                        console.log("Erreur de clear :" + err);
+                    });
+                }
+            }
+        }
+    }       
+});
+
+Client.on("message", message => {     
+    if(message.member.hasPermission("MANAGE_MESSAGES")){ 
+        if(message.content.startsWith(prefix + "mute")){
+            let mention = message.mentions.members.first();
+
+            if(mention == undefined){
+                message.reply("Membre mal ou non mentionné");
+            }
+            else {
+                mention.roles.add("830042980056039424");
+                message.channel.send(mention.displayName + " mute avec succès");
+            }
+        }
+        else if(message.content.startsWith(prefix + "unmute")){
+            let mention = message.mentions.members.first();
+            
+            if(mention == undefined){
+                message.reply("Membre mal ou non mentionné");
+            }
+            else {
+                mention.roles.remove("830042980056039424");
+                message.channel.send(mention.displayName + " unmute avec succès"); 
+            }
+        }
+        else if(message.content.startsWith(prefix + "tempmute")){
+            let mention = message.mentions.members.first();
+            
+            if(mention == undefined){
+                message.reply("Membre non ou mal mentionné");
+            }
+            else {
+                let args = message.content.split(" ");
+
+                mention.roles.add("830042980056039424");
+                setTimeout(function() {
+                    mention.roles.remove("830042980056039424");
+                    message.channel.send("<@" + mention.id + "> tu peux désormais parler de nouveau !");
+                }, args[2] * 1000);
+            }
+        }
+    }
+});
+
+Client.login(process.env.TOKEN);
